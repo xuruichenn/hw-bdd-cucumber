f6cb135c3628a3d02b208f19b0ac988126440974
Xurui Chen <xuruichen@berkeley.edu>
2020-11-03 21:58:18 +0000

    part 1, stuck on check/uncheck

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create! movie
   end
-  fail "Unimplemented"
+  
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +19,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  assert(page.body.index(e2)>page.body.index(e1))
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,7 +30,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list_split = rating_list.split(',')
+
+  rating_list_split.each do |r|
+      if uncheck
+          uncheck(r)
+      else
+          check(r)
+  end
+      
 end
 
 Then /I should see all the movies/ do
